35c4000ffcac486389c5f360b6d305684d7d736a
Alyson Campos <alyson.siqueiracampos@gmail.com>
2023-08-29 21:18:19 -0300

    att project

--- src/test/java/br/pe/acampos/SauceDemoTest.java
+++ src/test/java/br/pe/acampos/SauceDemoTest.java
@@ -55,7 +55,7 @@ public class SauceDemoTest {
     }
 
     @Test
-    @DisplayName("Nao deve logar sem passwordd")
+    @DisplayName("Nao deve logar sem password")
     public void naoDeveLogarSemPassword() {
         WebDriver driver = acessarAplicacao();
 
